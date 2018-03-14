fd6cf6cb49b4e9f72adddc3f45e43d9d03ad2e3c
Alex Muller de Jesus Sousa <a_lexmiller@hotmail.com>
2018-03-14 14:46:34 -0300

    carregando apps dinamicos atraves de arquivo manifest.json

--- src/root-application/root-application.js
+++ src/root-application/root-application.js
@@ -1,35 +1,15 @@
 import * as singleSpa from 'single-spa';
 
-// singleSpa.registerApplication('app1', () => import ('../app1/app1.js'), pathPrefix('/app1'));
-// singleSpa.registerApplication('app2', () => import ('../app2/app2.js'), pathPrefix('/app2'));
-
 var apps = require('./kroton-manifest.json');
 
-apps.forEach(function(app, index) {
-  singleSpa.registerApplication(app.name, 
-    () => System.import(`${app.file}`), pathPrefix(app.path));
+apps.forEach(function(app) {
+  singleSpa.registerApplication(
+    app.name, 
+      () => System.import(`../${app.name}/${app.file}`), 
+      (location) => location.pathname.startsWith(`${app.path}`));
 });
-
-// singleSpa.declareChildApplication('login', System.import('../login/dist/main.bundle'), () => true)
 
 singleSpa.start();
 
-function pathPrefix(prefix) {
-  return function(location) {
-    return location.pathname.startsWith(`${prefix}`);
-  } 
-}
 
-function loadApp(path) {
-  return () => System.import(`${path}`);
-}
 
-// function loadApp(url) {
-//   return new Promise((resolve, reject) => {
-//     const script = document.createElement('script')
-//     script.src = url;
-//     script.onload = resolve;
-//     script.onerror = reject;
-//     document.head.appendChild(script)
-//   });
-// }
